f68942814159ed3ca1cba1cf98232a0f07059596
Aakkash-Muthukumar <aakkash7153@gmail.com>
2025-04-28 15:54:07 -0700

    Added University

---
+++ src/User.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+	protected String id; 
+	protected String name; 
+	protected String password; 
+	
+    public User(String id, String name, String password) { 
+        this.id       = id; 
+        this.name     = name; 
+        this.password = password; 
+    } 
+	 
+    public String getId() { 
+    	return id; 
+    } 
+    
+    public String getName() { 
+    	return name; 
+	} 
+    
+    public abstract String getRole(); 
+ 
+    public boolean authenticate(String pw) { 
+        return this.password.equals(pw); 
+    } 
+}
