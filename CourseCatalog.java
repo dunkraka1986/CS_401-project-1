48b8a76bcd85d5884a084cbd4874e3857a7f901e
dunkraka1986 <122960793+dunkraka1986@users.noreply.github.com>
2025-05-02 23:37:57 -0700

    Update CourseCatalog.java

--- CourseCatalog.java
+++ CourseCatalog.java
@@ -5,14 +5,17 @@ public class CourseCatalog{
 	private ArrayList<Course> catalog;
 	
 	// methods
+	// add course to catalog array list
 	public void addToCatalog(Course course) {
 		this.catalog.add(course);
 	}
-	
+
+	// remove course from catalog arraly list
 	public void removeFromCatalog(Course course) {
 		this.catalog.remove(course);
 	}
-	
+
+	// get catalog array list
 	public ArrayList<Course> getCatalog() {
 		return this.catalog;
 	}
